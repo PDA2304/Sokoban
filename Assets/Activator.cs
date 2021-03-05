3f5394decdbb0e42d2b8eea7e07100c79ac4c5c3
PDA2304 <isip_d.a.pahomov@mpt.ru>
2021-03-05 19:02:31 +0300

    Socoban

---
+++ Assets/Activator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject[] firstGroup;
+    public GameObject[] secondGroup;
+    public Activator button;
+    public Material normal;
+    public Material transparent;
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.CompareTag("cube") || other.CompareTag("Player")){
+            foreach (GameObject first in firstGroup)
+            {
+                first.GetComponent <Renderer> ().material = normal;
+                first.GetComponent <Collider> ().isTrigger = false;
+            }
+            foreach (GameObject second in secondGroup)
+            {
+                second.GetComponent <Renderer> ().material = transparent;
+                second.GetComponent <Collider> ().isTrigger = true;
+            }
+            GetComponent  <Renderer> ().material = transparent;
+            button.GetComponent <Renderer> ().material = normal;
+        }
+
+    }
+}
